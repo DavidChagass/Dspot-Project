88d29e3edfe1015204d05b4826d83b69cc892d7d
devedeee <147016411+devedeee@users.noreply.github.com>
2024-05-16 17:29:09 -0300

    Update Empresa.php

--- Php/Empresa.php
+++ Php/Empresa.php
@@ -1,1 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+include 'Conexao.php';
+$email = "david@email.com";
+$senha = "david123";
+    function email($email)
+    {
+        $conn =  Database();
+
+        if (isset($email)) {
+            $sql = "SELECT idFuncionario FROM Funcionario WHERE emailFuncionario = {$email}";
+            $result = $conn->query($sql);
+        } else {
+            echo "<h1>" . "error email" . "</h1>";
+        }
+        echo $sql;
+    }
+    function senha($senha)
+    {
+        $conn = Database();
+        if (isset($senha)) {
+            $sql = "SELECT idFuncionario FROM Funcionario WHERE senhaFuncionario = {$senha}";
+            $result = $conn->query($sql);
+            
+        } else {
+            echo "<h1>" . "error senha" . "</h1>";
+        }
+    }
+
+?>
+
+</body>
+</html>
 
