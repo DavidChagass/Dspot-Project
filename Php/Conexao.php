bb0406c27595e18d2fe7147e0c5da752589657b6
devede <dvdpirateadok@gmail.com>
2024-05-16 08:57:00 -0300

    Funcionarios e conexao

--- Php/Conexao.php
+++ Php/Conexao.php
@@ -1,7 +1,24 @@
 <?php
      function Database(){
         try{
-            $mysql = new mysqli('localhost','root','','Medicine');            
-        }catch(Exception $e){ die("banco não encontado: " . $e->getMessage());}
+            $hostname = "localhost";
+            $username = "root";
+            $password = "";
+            $database = "Medicine";
+            $mysql = new mysqli( $hostname, $username, $password, $database);            
+            return $mysql;
+        }catch(Exception $e){ die("<h2>" . "banco não encontado: " . $e->getMessage() . "</h2>" );}
     }
+    
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    bom dia
+</body>
+</html>
