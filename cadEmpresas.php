a6cd8dfcaf6c417b992124fe48b1c46548889ede
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-05-17 15:07:48 -0300

    Update cadEmpresas.php

--- cadEmpresas.php
+++ cadEmpresas.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/normalize.css">
-    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/cadEmp.css">
     <script defer src="/js/main.js"></script>
     <link href="https://fonts.cdnfonts.com/css/berlin-sans-fb" rel="stylesheet">
     <title>Medicine-Bank</title>
