c6a7e9649f07ad0d07e28aa4dabb0d280d6a53f9
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-05-17 22:01:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="/css/main.css">
     <script defer src="/js/main.js"></script>
     <link href="https://fonts.cdnfonts.com/css/berlin-sans-fb" rel="stylesheet">
-    <title>Medicine-Bank - Funcionário</title>
+    <title>Dspot - Funcionário</title>
 </head>
 
 <body>
