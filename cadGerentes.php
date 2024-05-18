94e8776cc7079636c8ca6e22fb2642974b21e556
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-05-17 22:02:12 -0300

    Update cadGerentes.php

--- cadGerentes.php
+++ cadGerentes.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="/css/cadEmp.css">
     <script defer src="/js/main.js"></script>
     <link href="https://fonts.cdnfonts.com/css/berlin-sans-fb" rel="stylesheet">
-    <title>Medicine-Bank</title>
+    <title>Dspot - Gerentes</title>
 </head>
 
 <body>
