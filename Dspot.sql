19f7e4b404e75059928bb9a61fdb7d64f8c14c8e
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-05-19 21:35:58 -0300

    update Dspot.sql

---
+++ Dspot.sql
@@ -0,0 +1,34 @@
+drop database dspot;
+create database dspot;
+use dspot;
+create table Empresa(
+idEmpresa int primary key auto_increment,
+dominio char(10) not null,
+nomeEmpresa varchar(80) not null,
+cnpjEmpresa char(18) not null
+)engine=InnoDB;
+
+create table Funcionario(
+idFuncionario int primary key auto_increment not null,
+fk_idEmpresa int not null,
+nomeFuncionario varchar(200) not null,
+cpfFuncionario char(14) not null,
+senhaFuncionario varchar(20) not null,
+emailFuncionario varchar(150) not null,
+foreign key(fk_idEmpresa) references empresa(idEmpresa) on delete restrict on update restrict
+)engine=InnoDB;
+
+create table Gerente(
+idGerente int primary key auto_increment,
+fk_idEmpresa int not null,
+nomeGerente varchar(200) not null,
+senhaGerente varchar(20) not null,
+emailGerente varchar(150) not null,
+foreign key(fk_idEmpresa) references empresa(idEmpresa) on delete restrict on update restrict
+)engine=InnoDB;
+
+insert into Empresa(dominio, nomeEmpresa, cnpjEmpresa)
+values('12345-12*3', 'empresa', "123456789123456789");
+insert into Funcionario(fk_idEmpresa ,nomeFuncionario, cpfFuncionario, senhaFuncionario, emailFuncionario)
+values(1, "samuel silva", "063.513.480-21", "samuelsilva", "sanduicheiche@gmail.com");
+select * from empresa;
