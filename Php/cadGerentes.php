c0def119b45292d2e2de95071ac3bc962cc2189b
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-05-27 13:34:21 -0300

    Update and rename cadGerentes.php to Php/cadGerentes.php

--- Php/cadGerentes.php
+++ Php/cadGerentes.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/normalize.css">
-    <link rel="stylesheet" href="/css/cadEmp.css">
-    <script defer src="/js/main.js"></script>
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/cadEmp.css">
+    <script defer src="../js/main.js"></script>
     <link href="https://fonts.cdnfonts.com/css/berlin-sans-fb" rel="stylesheet">
 
     <title>Dspot - Gerentes</title>
@@ -16,7 +16,7 @@
     <!--[if lt IE 9]>
 	    <script src="bower_components/html5shiv/dist/html5shiv.js"></script>
     <![endif]-->
-    <form action="Php/Gerentes.php" method="post">
+    <form action="../Php/Gerentes.php" method="post">
         <div class="conteiner">
             <input type="text" required="required" onchange="domn()" name="dominio" class="dominio" maxlength="8" placeholder="Domínio">
         </div>
