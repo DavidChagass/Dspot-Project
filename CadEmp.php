ba5b6833fb5f77bf0efe584e87720a90688a4d1d
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-05-27 13:35:24 -0300

    Update CadEmp.php

--- CadEmp.php
+++ CadEmp.php
@@ -4,10 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/normalize.css">
-    <link rel="stylesheet" href="./css/cadEmp.css">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/cadEmp.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script defer src="./js/cadEmp.js"></script>
+    <script defer src="../js/cadEmp.js"></script>
     <link href="https://fonts.cdnfonts.com/css/berlin-sans-fb" rel="stylesheet">
     <title>Dspot - Empresas</title>
 </head>
