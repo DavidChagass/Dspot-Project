28cd21c038d50329d6c2042d9c775dc6b249985a
devede <dvdpirateadok@gmail.com>
2024-05-27 22:07:24 -0300

    conexao.php/funcionarios.php

--- Php/Conexao.php
+++ Php/Conexao.php
@@ -1,12 +1,3 @@
 <?php
-    $usuario = 'root';
-    $senha = '';
-    $database = 'Dspot';
-    $host = 'localhost';
-
-    $mysqli = new mysqli($host, $usuario, $senha, $database);
-    return $mysqli;
-    if ($mysqli->error) {
-        die("Falha ao conectar ao banco de dados: " . $mysqli->error);
-    }
+$pdo = new PDO('mysql:host=localhost;dbname=dspot', 'root','');
 ?>
--- Php/Funcionarios.php
+++ Php/Funcionarios.php
@@ -1,3 +1,33 @@
+<?php
+
+include('Conexao.php');
+
+// $nomeFuncionario = $_POST['nomeFuncionario'];
+// $nomeEmpresa = $_POST['nomeEmpresa'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$dominio = $_POST['dominio'];
+
+try{
+if (isset($_POST['email']) && isset($_POST['senha']) && isset($_POST['dominio'])) {
+
+    $sql_code = "SELECT * FROM Funcionario INNER JOIN Empresa ON(Funcionario.fk_idEmpresa = Empresa.idEmpresa) WHERE Empresa.dominio = '{$dominio}' AND Funcionario.emailFuncionario = '{$email}' AND Funcionario.senhaFuncionario = '{$senha}'";
+    $stmt = $pdo->prepare($sql_code);
+    $stmt->execute();
+    $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $sql_query = $stmt->rowCount();
+
+
+} else {
+    echo "senha e email vazios";
+}
+}catch(PDOException $e){
+    $sql_query . $e->getMessage();
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -12,33 +42,13 @@
 
 <body>
 
-    <?php
 
-    include('Conexao.php');
-    $email = $_POST['email'];
-    $senha = $_POST['senha'];
-    $dominio = $_POST['dominio'];
+<?php 
 
+    if($sql_query){
+        foreach ($resultado as $linha) {
 
-    if (isset($_POST['email']) && isset($_POST['senha']) && isset($_POST['dominio'])) {
-
-        $dominio = $mysqli->real_escape_string($_POST['dominio']);
-        $email = $mysqli->real_escape_string($_POST['email']);
-        $senha = $mysqli->real_escape_string($_POST['senha']);
-
-        $sql_code = "SELECT * FROM Funcionario INNER JOIN Empresa ON(Funcionario.fk_idEmpresa = Empresa.idEmpresa) WHERE Empresa.dominio = '$dominio' AND Funcionario.emailFuncionario = '$email' AND Funcionario.senhaFuncionario = '$senha'";
-      
-        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código mysql: " . $mysqli->error);
-
-
-    } else {
-        echo "senha e email vazios";
-    }
-
-    foreach ($sql_query as $linha) {
-
-
-    ?>
+?>
         <table>
 
             <tr>
@@ -72,9 +82,9 @@
                 </td>
             </tr>
         </table>
-    <?php } ?>
-    <section><h2>BEM VINDO AO MENU DA EMPRESA <?= strtoupper($linha['nomeFuncionario']) ?></h2></section>
 
+    <section><h2>BEM VINDO AO MENU DA EMPRESA <?= strtoupper($linha['nomeFuncionario']) ?></h2></section>
+    <?php } }else{echo "erro ao mostrar";}?>
     
 </body>
 </html>
