59e967fb89c95612b5d6f8eed8dcf24c9e716c29
Ravii2709 <124733552+Ravii2709@users.noreply.github.com>
2024-06-14 14:58:06 -0300

    Update Pessoa.php

--- DTO/Pessoa.php
+++ DTO/Pessoa.php
@@ -1,15 +1,16 @@
 <?php
 class Pessoa{
+    //atributos do objeto
     private $dominio;
     private $senha;
     private $email;
-
+//metodo construtor
     public function __construct($dominio, $senha, $email){
         $this->dominio = $dominio;
         $this->senha = $senha;
         $this->email = $email;
     }
-
+//funcoes do objeto
     public function login(){
         $this->dominio;
         $this->senha;
