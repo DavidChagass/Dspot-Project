707f2fb26538ea4e460b3a4c1b796233ea078c13
devede <dvdpirateadok@gmail.com>
2024-06-21 19:29:44 -0300

    Update GerentesBLL.php

--- BLL/GerentesBLL.php
+++ BLL/GerentesBLL.php
@@ -4,21 +4,29 @@ require_once '../DTO/GerenteDTO.php';
 
 class GerentesBLL extends GerenteDTO
 {
-    public static function cadfuncionario($idempresa,$nomeFuncionario, $email, $senha ) {
-            $connObj = new Conexao();
-            $conn = $connObj->retornaConexao();
-            $sql_code = "INSERT INTO Funcionario (nomeFuncionario, emailFuncionario, senhaFuncionario, fk_idEmpresa) 
+    public static function cadfuncionario($idempresa, $nomeFuncionario, $email, $senha)
+    {
+        $connObj = new Conexao();
+        $conn = $connObj->retornaConexao();
+        $sql_code = "INSERT INTO Funcionario (nomeFuncionario, emailFuncionario, senhaFuncionario, fk_idEmpresa) 
                        VALUES (:nomeFuncionario, :email, :senha, :idEmpresa)";
 
-            $stmt = $conn->prepare($sql_code);
-            $stmt->bindParam(':idEmpresa', $idempresa);
-            $stmt->bindParam(':nomeFuncionario', $nomeFuncionario);
-            $stmt->bindParam(':email', $email);
-            $stmt->bindParam(':senha', $senha);
-            if($stmt->execute()){
-                return "funcionario inserido";
-            }else{echo "erro ao inserir";}
+        $stmt = $conn->prepare($sql_code);
+        $stmt->bindParam(':idEmpresa', $idempresa);
+        $stmt->bindParam(':nomeFuncionario', $nomeFuncionario);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':senha', $senha);
+        if ($stmt->execute()) {
+            return "funcionario inserido";
+        } else {
+            echo "erro ao inserir";
+        }
+    }
 
-}
+    public static function ContFuncionario(){
+        
+    }
+
+
 
 }
