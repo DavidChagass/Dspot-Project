5789ee7fa9e17d69dcae76532e2a834c376f7e91
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-07-03 23:40:18 -0300

    Ui/Ux Funcionários

--- View/Funcionarios.php
+++ View/Funcionarios.php
@@ -7,6 +7,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $senha = $_POST['senha'];
     $resultados = DaoFuncionarios::Login($dominio, $email, $senha);
 }
+if ($resultados) {
+    $dominio = htmlspecialchars($resultados['dominio']);
+    $nome = htmlspecialchars($resultados['nomeFuncionario']);
+    $email = htmlspecialchars($resultados['emailFuncionario']);
+    $senha = htmlspecialchars($resultados['senhaFuncionario']);
+} else {
+    echo "Login inválido";
+}
 ?>
 
 <!DOCTYPE html>
@@ -15,46 +23,75 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Funcionario</title>
-    <link rel="stylesheet" href="../css/main.css">
-    <link rel="stylesheet" href="../css/Funcionario.css">
+    <title><?= $nome ?> - Dspot</title>
+    <link rel="stylesheet" href="./css/normalize.css">
+    <link rel="stylesheet" href="../css/funcionario.plataform.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <?php
-    if ($resultados) {
-        $dominio = htmlspecialchars($resultados['dominio']);
-        $nome = htmlspecialchars($resultados['nomeFuncionario']);
-        $email = htmlspecialchars($resultados['emailFuncionario']);
-        $senha = htmlspecialchars($resultados['senhaFuncionario']);
-    ?>
-        <table>
-            <tr>
-                <td>
-                    <h3>Dominio: <?= $dominio ?> </h3>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <h3>Nome do funcionario: <?= $nome ?> </h3>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <h3>Email: <?= $email ?> </h3>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <h3>Senha:<?= $senha ?> </h3>
-                </td>
-            </tr>
-        </table>
-    <?php
-    } else {
-        echo "Login inválido";
-    }
-    ?>
+    <!--[if lt IE 9]>
+	    <script src="bower_components/html5shiv/dist/html5shiv.js"></script>
+    <![endif]-->
+    <div class="container-fluid mainContainer">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="../img/Logo.png" alt="Bootstrap" width="130" height="28">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Assinatura</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Cargos
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Funcionário</a></li>
+                                <li><a class="dropdown-item" href="#">Gerente</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="#">Empresa</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Produto" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Buscar</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <div class="container-fluid produtos">
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Nome Produto</h5>
+                    <p class="card-text">Descrição breve sobre o produto</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Marca - ....</li>
+                    <li class="list-group-item">Quantidade Atual - .......</li>
+                    <li class="list-group-item">Ultima saída - ......</li>
+                </ul>
+                <form class="control d-flex">
+                    <button type="button" class="btn btn-info">Adicionar</button>
+                    <input type="number" class="form-control productNumber" placeholder="0" aria-describedby="addon-wrapping">
+                    <button type="button" class="btn btn-info">Retirar</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
