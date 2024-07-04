ae4114f104c82ff8f1cb9916753a786043f54ccc
Caik Lima <124724369+Caik0@users.noreply.github.com>
2024-07-04 14:28:19 -0300

    Js mask for the company domain

--- View/CadEmp.php
+++ View/CadEmp.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="../css/normalize.css">
     <link rel="stylesheet" href="../css/cadEmp.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script defer src="../js/cadEmp.js"></script>
+    <script defer src="../js/CpfCnpj.validate.js"></script>
     <link href="https://fonts.cdnfonts.com/css/berlin-sans-fb" rel="stylesheet">
     <title>Dspot - Empresas</title>
 </head>
